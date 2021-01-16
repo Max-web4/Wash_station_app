6e7c4e7554a04ab3754672b4160c7079480f984e
Max <yudinmax4444@gmail.com>
2021-01-16 10:22:32 +0200

    plug-in__maps

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// function initMap() {
+//     var coordinates =  {lat: 49.2330278307403, lng: 28.473913635255332
+// },
+    
+//         map = new google.maps.Map(document.getElementById('map'), {
+//             center: coordinates
+//         });
+// }
